85ce1a0037c9348cd3add0d74316c1178a63005b
Romil Shah <romil.shah@photon.com>
2024-05-09 11:34:01 -0500

    6

--- src/main1/java/GroovyASTParser.java
+++ src/main1/java/GroovyASTParser.java
@@ -1,71 +1,51 @@
 package main1.java;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import groovy.lang.GroovyShell;
-import groovy.lang.Script;
-import org.codehaus.groovy.ast.ModuleNode;
+import groovy.lang.GroovyClassLoader;
 import org.codehaus.groovy.control.CompilerConfiguration;
-import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.control.CompilationUnit;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.tools.GroovyClass;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
-public class GroovyASTParser {
-
+public class GroovyASTConverter {
     public static void main(String[] args) {
+        String inputFilePath = "path/to/your/configfile.config";
+        String outputFilePath = "path/to/your/outputfile.ast";
+
+        convertGroovyCodeToAST(inputFilePath, outputFilePath);
+    }
+
+    private static void convertGroovyCodeToAST(String inputFilePath, String outputFilePath) {
         try {
-            String groovyFilePath = "/path/to/your/file.groovy"; // Path to Groovy file
-            String jsonFilePath = "/path/to/your/file.json"; // Path to output JSON file
+            // Read the content of the Groovy file
+            String groovyCode = new String(Files.readAllBytes(Paths.get(inputFilePath)));
 
-            // Read Groovy file
-            String scriptContent = new String(Files.readAllBytes(Paths.get(groovyFilePath)));
+            // Configure the compiler
+            CompilerConfiguration config = new CompilerConfiguration();
+            config.setSourceEncoding("UTF-8");
 
-            // Parse Groovy script to AST
-            ModuleNode moduleNode = parseGroovyScriptToAST(scriptContent);
+            // Create a CompilationUnit
+            CompilationUnit compilationUnit = new CompilationUnit(config);
+            compilationUnit.addSource("GroovyConfig", groovyCode);
+            compilationUnit.compile(); // Compile through all phases
 
-            // Configure ObjectMapper and register custom serializer
-            ObjectMapper mapper = new ObjectMapper();
-            SimpleModule module = new SimpleModule();
-            module.addSerializer(ModuleNode.class, new ModuleNodeSerializer());
-            mapper.registerModule(module);
+            // Retrieve all classes in the CompilationUnit
+            List<GroovyClass> classes = compilationUnit.getClasses();
+            try (FileWriter writer = new FileWriter(outputFilePath)) {
+                for (GroovyClass classNode : classes) {
+                    writer.write(classNode.toString());
+                    writer.write("\n");
+                }
+            }
 
-            // Serialize AST to JSON
-            String json = mapper.writeValueAsString(moduleNode);
-
-            // Save JSON to file
-            Files.write(Paths.get(jsonFilePath), json.getBytes());
-
-            System.out.println("AST JSON has been saved successfully!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static ModuleNode parseGroovyScriptToAST(String scriptContent) {
-        CompilerConfiguration config = new CompilerConfiguration();
-        config.setScriptBaseClass(Script.class.getName());
-
-        GroovyShell shell = new GroovyShell(config);
-        SourceUnit sourceUnit = SourceUnit.create("GroovyScript", scriptContent);
-        sourceUnit.parse();
-        sourceUnit.completePhase();
-        sourceUnit.convert();
-        return sourceUnit.getAST();
-    }
-
-    static class ModuleNodeSerializer extends JsonSerializer<ModuleNode> {
-        @Override
-        public void serialize(ModuleNode value, JsonGenerator gen, SerializerProvider serializers) throws IOException, IOException {
-            gen.writeStartObject();
-            gen.writeStringField("name", value.getPackageName());
-            // Add more fields as necessary, handling each carefully to avoid recursion
-            gen.writeEndObject();
+        } catch (IOException e) {
+            System.err.println("Error handling file: " + e.getMessage());
         }
     }
 }
